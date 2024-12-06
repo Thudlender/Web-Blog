c2b91b51e369ea17ee934832dd7fb9e1f6ad2a1f
Thudlender <tthunder1688@gmail.com>
2024-12-06 17:04:33 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import LoginBtn from './LoginBtn';
+import RegisterBtn from './RegisterBtn';
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">Web Blog</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Link</a>
+            </li>
+            <div className="flex space-x-4 ">
+              {/* Register and Login Buttons */}
+              <RegisterBtn />
+              <LoginBtn />
+            </div>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
+
