b97758d8713adb15e93347a5335388d26b2c03ff
Thudlender <tthunder1688@gmail.com>
2024-12-12 12:16:51 +0700

    Added backend err

---
+++ Backend/controllers/user.controller.js
@@ -0,0 +1,30 @@
+const bcrypt = require("bcrypt");
+const UserModel = require("../models/User");
+const salt = bcrypt.genSaltSync(10);
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || password) {
+    res.status(400).send({
+      message: "Please provide all require fields!",
+    });
+    return;
+  }
+  try {
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    const user = await UserModel.create({
+      username,
+      password: hashedPassword,
+    });
+    res.send({
+      message: "User registered Successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message:
+        error.message ||
+        "Something error occurred while registering a new user",
+    });
+  }
+};
---
+++ Backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const BASE_URL = process.env.BASE_URL;
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
+const userRouter = require("./routers/user.router");
+
+//Connect to Mongo DB
+try {
+  mongoose.connect(DB_URL);
+  console.log("Connect to Mongo DB Successfully");
+} catch (error) {
+  console.log("DB Connection Failed");
+}
+
+app.use(cors({ origin: BASE_URL, credentials: true }));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to SE NPRU Blog Restful API</h1>");
+});
+
+//use Router
+app.use("/api/v1/auth", userRouter);
+
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:" + PORT);
+});
